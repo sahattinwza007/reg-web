0064ed533c04a769022451519ceeed6057920f42
Sahatsawat Pinta <56186363+sahattinwza007@users.noreply.github.com>
2021-08-03 20:33:22 +0700

    build reg web
    first build

---
+++ index.js
@@ -0,0 +1,72 @@
+// Import stylesheets
+import './style.css';
+
+// Body element
+const body = document.getElementById('body');
+
+// Button elements
+const btnSend = document.getElementById('btnSend');
+const btnClose = document.getElementById('btnClose');
+const btnShare = document.getElementById('btnShare');
+const btnLogIn = document.getElementById('btnLogIn');
+const btnLogOut = document.getElementById('btnLogOut');
+const btnScanCode = document.getElementById('btnScanCode');
+const btnOpenWindow = document.getElementById('btnOpenWindow');
+
+// Profile elements
+const email = document.getElementById('Email');
+const userId = document.getElementById('userId');
+const pictureUrl = document.getElementById('pictureUrl');
+const displayName = document.getElementById('displayName');
+const statusMessage = document.getElementById('statusMessage');
+
+// QR element
+const code = document.getElementById('code');
+const friendShip = document.getElementById('friendShip');
+
+async function main() {
+  // Initialize LIFF app)
+  switch (liff.getOS()) {
+    case 'android':
+      body.style.backgroundColor = '#d1f5d3';
+      break;
+    case 'ios':
+      body.style.backgroundColor = '#eeeeee';
+      break;
+  }
+
+  // Try a LIFF function
+  await liff.init({liffId: '1656129299-PLzNKdpa' });
+
+  if (!liff.isInClient()) {
+    if (liff.isLoggedIn()) {
+      btnLogIn.style.display = "none"
+      btnLogOut.style.display = "block"
+      getUserProfile()
+    } else {
+      btnLogIn.style.display = "block"
+      btnLogOut.style.display = "none"
+    }
+  } else {
+    getUserProfile()
+  }
+}
+main();
+
+async function getUserProfile() {
+  const profile = await liff.getProfile();
+  pictureUrl.src = profile.pictureUrl;
+  userId.innerHTML = '<b>userId:</b> ' + profile.userId;
+  statusMessage.innerHTML = '<b>statusMessage:</b> ' + profile.statusMessage;
+  displayName.innerHTML = '<b>displayName:</b> ' + profile.displayName;
+  email.innerHTML = '<b>email:</b> ' + liff.getDecodedIDToken().email;
+}
+
+btnLogIn.onclick = () => {
+  liff.login()
+}
+
+btnLogOut.onclick = () => {
+  liff.logout()
+  window.location.reload()
+}
