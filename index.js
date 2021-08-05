0a61b6dc360869f3878884ea916ce74e4fd6ce53
Sahatsawat Pinta <56186363+sahattinwza007@users.noreply.github.com>
2021-08-05 20:41:52 +0700

    firebase

--- index.js
+++ index.js
@@ -21,6 +21,42 @@ function logOut() {
   window.location.reload()
 }
 
+let userList = document.querySelector('#userList');
+let form = document.querySelector('#addUser');
+
+function renderUser(doc) {
+    let li = document.createElement('li');
+    let firstname = document.createElement('span');
+    let lastname = document.createElement('span');
+    let studentid = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    firstname.textContent = doc.data().firstname;
+    lastname.textContent = doc.data().lastname;
+    studentid.textContent = doc.data().studentid;
+
+    li.appendChild(firstname);
+    li.appendChild(lastname);
+    li.appendChild(studentid);
+
+    userList.appendChild(li);
+}
+
 db.collection('users').get().then(user => {
-  console.log(user);
-});
+  user.docs.forEach(doc => {
+      console.log(doc.data())
+      renderUser(doc);
+  })
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('users').add({
+      firstname: form.firstname.value,
+      lastname: form.lastname.value,
+      studentid: form.studentid.value,
+  })
+  form.firstname.value = '';
+  form.lastname.value = '';
+  form.studentid.value = '';
+})
